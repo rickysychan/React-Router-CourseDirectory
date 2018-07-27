7bea3172966285a9da68065fcb58cf01d636fc04
rickysychan <rickysychan@gmail.com>
2018-07-27 12:18:30 -0400

    fixed switch issue

--- src/components/App.js
+++ src/components/App.js
@@ -16,17 +16,17 @@ import Featured from './Featured'
 
 const App = () => (
   <BrowserRouter>
-  <div className="container">
-    <Header />
-    <Switch>
-      <Route exact path='/' component={Home} />
-      <Route path='/about' render={ () => { return(<About title='About' />) } } />
-      <Route exact path='/teachers' component={Teachers} />
-      <Route path='/teachers/:topic/:name' component={Featured} />
-      <Route path='/foo' component={Courses} />
-      <Route component={NotFound} />
-    </Switch>
-  </div>
+    <div className="container">
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/about' render={ () => { return(<About title='About' />) } } />
+        <Route exact path='/teachers' component={Teachers} />
+        <Route path='/teachers/:topic/:name' component={Featured} />
+        <Route path='/foo' component={Courses} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
   </BrowserRouter>
 );
 
